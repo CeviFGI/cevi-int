1d8d4faa46d310bb3d596f6e9ab05ebcf4e9cd43
Patrick Uhlmann <github@uhlme.ch>
2023-01-07 20:25:48 +0100

    alter column doesn't work on mysql
    switch to modify

--- src/main/resources/db/migration/V1.0.2__TableNotNullConstraints.sql
+++ src/main/resources/db/migration/V1.0.2__TableNotNullConstraints.sql
@@ -1,13 +1,15 @@
-ALTER TABLE events ALTER COLUMN date SET NOT NULL;
-ALTER TABLE events ALTER COLUMN title SET NOT NULL;
-ALTER TABLE events ALTER COLUMN description SET NOT NULL;
-ALTER TABLE events ALTER COLUMN location SET NOT NULL;
+ALTER TABLE events MODIFY date varchar(255) NOT NULL;
+ALTER TABLE events MODIFY description text NOT NULL;
+ALTER TABLE events MODIFY location varchar(255) NOT NULL;
+ALTER TABLE events MODIFY title varchar(255) NOT NULL;
 
-ALTER TABLE voluntary_services ALTER COLUMN organization SET NOT NULL;
-ALTER TABLE voluntary_services ALTER COLUMN organizationLink SET NOT NULL;
-ALTER TABLE voluntary_services ALTER COLUMN description SET NOT NULL;
-ALTER TABLE voluntary_services ALTER COLUMN location SET NOT NULL;
+ALTER TABLE exchanges MODIFY description text NOT NULL;
+ALTER TABLE exchanges MODIFY organization varchar(255) NOT NULL;
+ALTER TABLE exchanges MODIFY organizationLink varchar(255) NOT NULL;
 
-ALTER TABLE exchanges ALTER COLUMN organization SET NOT NULL;
-ALTER TABLE exchanges ALTER COLUMN organizationLink SET NOT NULL;
-ALTER TABLE exchanges ALTER COLUMN description SET NOT NULL;
+ALTER TABLE voluntary_services MODIFY description text NOT NULL;
+ALTER TABLE voluntary_services MODIFY location varchar(255) NOT NULL;
+ALTER TABLE voluntary_services MODIFY organization varchar(255) NOT NULL;
+ALTER TABLE voluntary_services MODIFY organizationLink varchar(255) NOT NULL;
+
+ALTER TABLE contact_form_entries MODIFY message varchar(255)  NOT NULL;
