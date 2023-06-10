26d40c1f8cd19be13991d412c3842a3d9d99ef5d
Patrick Uhlmann <github@uhlme.ch>
2023-06-11 00:44:53 +0200

    bump dependencies
    and make tests independent from date

--- src/main/java/tools/cevi/infra/DemoData.java
+++ src/main/java/tools/cevi/infra/DemoData.java
@@ -12,7 +12,6 @@ import tools.cevi.exchange.Exchange;
 import tools.cevi.voluntary.VoluntaryService;
 
 import java.time.LocalDate;
-import java.time.Month;
 
 @ApplicationScoped
 @SuppressWarnings("unused")
@@ -22,10 +21,10 @@ public class DemoData {
         if (Event.count() == 0) {
             Log.info("Database is empty. Generating some demo data...");
             createEvent("Past Event", "In der Vergangenheit", "Antwerp", "Generalversammlung des YMCA", LocalDate.now().minusDays(1));
-            createEvent("Olivenernte", "15. - 24. Oktober 2023", "Palästina", "Unterstützt durch Horyzon. Kosten: 900 - 1'200 USD je nach Unterkunft + Reise. Das Programm ist eine internationale Solidaritäts- und Schutzaktion für die palästinensischen Bauern und Familien, um sie bei der Ernte ihrer Olivenbäume zu unterstützen, die von Angriffen, Entwurzlung, Beschlagnahmungen und Einschränkungen bedroht sind. Neben der Olivenernte umfasst das Programm auch Führungen in verschiedenen Städten, palästinensischer Kultur und einführernde Vorträge zu Konfliktthemen.<br><a href=\"https://www.jai-pal.org/en/campaigns/olive-tree-campaign/olive-picking-program/picking2022-inv\">Weitere Informationen</a>", LocalDate.of(2023, Month.OCTOBER, 15));
-            createEvent("European Unify Conference", "20. - 23. Oktober 2023", "Wien", "Fest zu den christlichen Wurzeln des Cevi. Kosten: 95 Euro Festival + Reise + Unterkunft.<br><a href=\"https://www.ymca-unify.eu/european/unify_2022\">Weitere Informationen</a>, <a href=\"https://mcusercontent.com/4164786c200962ea4be64ffd8/files/20dfc6b4-c4f8-19fe-6006-d42a99a47a60/Program_Unify_2022.pdf\">Programm</a>", LocalDate.of(2023, Month.OCTOBER, 20));
-            createEvent("YE 50th anniversary", "28. Juni - 02. Juli 2023", "Berlin", "Es werden ca. 500 Personen erwartet. Detailinformationen folgen Ende September/Anfang Oktober.", LocalDate.of(2023, Month.JUNE, 28));
-            createEvent("YMCA General Assembly", "Mai 2023", "Antwerp", "Generalversammlung des YMCA", LocalDate.of(2023, Month.JUNE, 1));
+            createEvent("Olivenernte", "15. - 24. Oktober 2023", "Palästina", "Unterstützt durch Horyzon. Kosten: 900 - 1'200 USD je nach Unterkunft + Reise. Das Programm ist eine internationale Solidaritäts- und Schutzaktion für die palästinensischen Bauern und Familien, um sie bei der Ernte ihrer Olivenbäume zu unterstützen, die von Angriffen, Entwurzlung, Beschlagnahmungen und Einschränkungen bedroht sind. Neben der Olivenernte umfasst das Programm auch Führungen in verschiedenen Städten, palästinensischer Kultur und einführernde Vorträge zu Konfliktthemen.<br><a href=\"https://www.jai-pal.org/en/campaigns/olive-tree-campaign/olive-picking-program/picking2022-inv\">Weitere Informationen</a>", LocalDate.now());
+            createEvent("European Unify Conference", "20. - 23. Oktober 2023", "Wien", "Fest zu den christlichen Wurzeln des Cevi. Kosten: 95 Euro Festival + Reise + Unterkunft.<br><a href=\"https://www.ymca-unify.eu/european/unify_2022\">Weitere Informationen</a>, <a href=\"https://mcusercontent.com/4164786c200962ea4be64ffd8/files/20dfc6b4-c4f8-19fe-6006-d42a99a47a60/Program_Unify_2022.pdf\">Programm</a>", LocalDate.now().plusDays(1));
+            createEvent("YE 50th anniversary", "28. Juni - 02. Juli 2023", "Berlin", "Es werden ca. 500 Personen erwartet. Detailinformationen folgen Ende September/Anfang Oktober.", LocalDate.now().plusDays(2));
+            createEvent("YMCA General Assembly", "Mai 2023", "Antwerp", "Generalversammlung des YMCA", LocalDate.now().plusDays(3));
 
             createVoluntaryService("Horyzon", "https://horyzon.ch/", "Kolumbien, Palästina",  "Dauer 2 Wochen oder 3-6 Monate. Siehe <a href=\"https://horyzon.ch/de/spenden-und-unterstuetzen/einsaetze-weltweit/volontariat/\">Weitere Informationen</a>");
             createVoluntaryService("Volunteers for Europe", "http://vfe.cvjm.de/", "Deutschland",  "Dauer 2 Wochen oder 3-6 Monate. Siehe Webseite von CVJM");
